bcb50f468bb7336c4d851674144d166b0a49f25f
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-08 16:37:32 +0100

    sistemazione footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,33 +1,66 @@
 <footer>
     <section class="sfondoFooter">
         <div class="container">
-            <div class="row">
-                <div class="col-6 text-white">
-                    <ul>
-                        <li>prova</li>
+            <div class="row py-4 position-relative">
+                <div class="col-6 text-secondary d-flex">
+                    <ul class="list-unstyled me-3">
+                        <li class="text-white fw-bold mb-3">DC COMICS</li>
+                        <li>characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                        <li class="text-white fw-bold my-3">SHOP</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
                     </ul>
-                    <ul>
-                        <li>prova</li>
+                    <ul class="list-unstyled me-3">
+                        <li class="text-white fw-bold mb-3">DC</li>
+                        <li>Term of Use</li>
+                        <li>Privacy polici(NEW)</li>
+                        <li>Ad choice</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact US</li>
                     </ul>
-                    <ul>
-                        <li>prova</li>
+                    <ul class="list-unstyled me-3">
+                        <li class="text-white fw-bold mb-3">SITES</li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
                     </ul>
                 </div>
-                <div class="col-6">
-                    <img src="../../assets/images/dc-logo-bg.png" alt="">
+                <p class="text-secondary m-0">All site content Tm and 2020 DC entertainment, unless otherwise <a class="text-decoration-none" href="@">noted here</a> . all rights reserved.</p>
+                <a class="text-decoration-none" href="@">Cookies Settings</a>
+                <div class="col-6 position-absolute">
+                    <img class="position-absolute" src="{{Vite::asset('resources/assets/images/dc-logo-bg.png')}}" alt="">
                 </div>
             </div>
         </div>
     </section>
     <section class="grigio">
         <div class="container">
-            <div class="row">
+            <div class="row py-4">
                 <div class="col-6">
-                    <a href="#" class="btn btn-primary">SIGN-UP NOW</a>
+                    <a href="#" class="btn btn-outline-primary text-white">SIGN-UP NOW</a>
                 </div>
                 <div class="col-6 text-end">
-                    <p>FOLLOW US</p>
-                </div>  
+                    <p class="text-primary m-0 me-3 d-inline-block">FOLLOW US</p>
+                    <img class="bg-dark rounded-circle" src="{{Vite::asset('resources/assets/images/footer-facebook.png')}}" alt="">
+                    <img class="bg-dark rounded-circle" src="{{Vite::asset('resources/assets/images/footer-twitter.png')}}" alt="">
+                    <img class="bg-dark rounded-circle" src="{{Vite::asset('resources/assets/images/footer-youtube.png')}}" alt="">
+                    <img class="bg-dark rounded-circle" src="{{Vite::asset('resources/assets/images/footer-pinterest.png')}}" alt="">
+                    <img class="bg-dark rounded-circle" src="{{Vite::asset('resources/assets/images/footer-periscope.png')}}" alt="">
+                </div>
             </div>
         </div>
     </section>

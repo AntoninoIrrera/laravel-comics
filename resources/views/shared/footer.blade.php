a2f1e397621e6ee3257d96e18b2377da8ffc6429
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-08 15:32:34 +0100

    inizio creazione layouts base e pagine shared

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,34 @@
+<footer>
+    <section class="sfondoFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 text-white">
+                    <ul>
+                        <li>prova</li>
+                    </ul>
+                    <ul>
+                        <li>prova</li>
+                    </ul>
+                    <ul>
+                        <li>prova</li>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <img src="../../assets/images/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="grigio">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <a href="#" class="btn btn-primary">SIGN-UP NOW</a>
+                </div>
+                <div class="col-6 text-end">
+                    <p>FOLLOW US</p>
+                </div>  
+            </div>
+        </div>
+    </section>
+</footer>
